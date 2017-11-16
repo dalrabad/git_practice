9a7e2d7a31fc85c9cdb64127199e36f83693dcfb
Dunia Alrabadi <mac@Macs-MacBook-Pro.local>
2017-11-15 20:28:49 -0700

    added color

--- main.rb
+++ main.rb
@@ -3,13 +3,13 @@ require 'git'
 
 class Mian 
     include Git
-    def menu
+    def self.mainmenu
         puts "1 Enter git command".colorize(:yellow)
         puts "2 Exit"
         choice = gets.to_i
         case choice
         when 1
-            puts "Enter git command".colorize(:red)
+            puts "Enter git command".colorize(:pink)
             puts_git(gets.strip)
             menu
         when 2 
